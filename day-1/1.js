e6c0d1c51bebe0099664de25353dfcf72fe4774b
sunitameena25 <sunitameena25@navgurkul.org>
2025-05-09 22:25:27 +0530

    completed js logical question

--- day-1/1.js
+++ day-1/1.js
@@ -1,1 +1,129 @@
-console.log("hello world");
+// Declare a variable to store your name and log it to the console.
+// let a;
+// a = "sunita";2025
+// console.log(a);
+
+//  Create variables for age, country, and hobby — display a sentence using all.->   20 india danse
+// var age = prompt("What is your age?");
+// var country = prompt("country name");
+// var hobby=prompt("hobby");
+// console.log(age,country,hobby);
+
+//  Swap two variables' values without using a third variable.  -> swap
+// let a=10;
+// let b=20;
+// a = a+b; //30
+// b = a-b;  //30-20=10
+// a = a-b;  //30-10=20
+// console.log(a,b);
+
+// // Reassign a let variable and explain why const can't be reassigned.
+// // let can be reassigned
+// let color ="red";
+// color ="blue";
+// console.log("color:",color);
+
+// //const cant't be
+// const a=20;
+// a=30;
+// console.log("a:",a);
+
+// Store the current year and your birth year, then calculate and display your age.
+//  let currentYear = 2025;
+//  let birthYear =1996;
+//  let age =currentYear-birthYear
+//  console.log(age);
+
+// Write a program to check if a number is even or odd using % operator.
+// let num = prompt("take anumber");
+// if (num%2 ===0){
+//     console.log("num is even");
+// }else{
+//     console.log("num is odd");
+
+// }
+
+ // Use comparison operators to check if a person is eligible to vote. 
+//    let age = prompt("take a age");
+//    if(age >=18){
+//      console.log("eligible to vote");
+//    }else{
+//      console.log("not elegible");
+//    }
+
+// Calculate total price with a discount using arithmetic operators. 
+    // let price = 1000;
+    // let discount = 10;
+    // let totalPrice = price-(price*discount/100);
+    // console.log(totalPrice);
+
+// Use logical operators to check if a user is both verified and logged in return true else false. 
+//    let userVeryfied = true;
+//    let isLoggedIn = true;
+//    if(userVeryfied && isLoggedIn){
+//      console.log("true");
+//    }else{
+//      console.log("false");
+//    }
+
+// Increment a variable by 1 using both ++ and += — show the difference.
+//  let count =5;
+//  count++;
+//  console.log(count);
+
+//  let score =4;
+//  score +=;
+//  console.log(count);
+   
+// Create a function that adds two numbers and returns the result.  
+//  let a=2;
+//  let b=4;
+// function add(){
+//     console.log(a+b);
+// }
+// add();
+
+// // Make a function that checks if a number is prime.
+// let num = prompt("type any number");
+// function prime(){
+//     if(num > 1)
+// }
+
+// Build a function that returns the maximum of three numbers. 
+//   let a=3;
+//   let b=5;
+//   let c=8;
+//   function max(){
+//     if(a>b && a>c){
+//         console.log("a is max");
+//     }else if(b>c && b>a){
+//         console.log("b is max");
+//     }else{
+//         console.log("c is max");
+//     }
+//   }
+//   max();
+
+// Create a function to reverse a string. 
+//   let string = "hello";
+//   let reversed = "";
+//   function reverse(){
+//     for(let  i=string.length-1; i>=0;i--){
+//         reversed+= string[i];
+//     }
+//     console.log(reversed);
+//   }
+//   reverse();
+
+// Write a function that counts the number of vowels in a string.  
+//  let string = "hello";
+//  let vowel = "";
+//  function countVowel(){
+//     for(let i=0; i<string.length; i++){
+//     if ("aeiou".includes(string[i].toLowerCase())) {
+//         vowel+= string[i];
+//         }
+//     }
+//     console.log(vowel);
+//  }
+//  countVowel();
